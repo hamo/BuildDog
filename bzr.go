7f6908ceef474e4770163394f847d8b6d72e3cc6
Yang Bai <hamo.by@gmail.com>
2014-04-09 20:25:30 +0800

    remove .bzr dir after branch

--- bzr.go
+++ bzr.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"path/filepath"
 	"io"
+	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 type bzrRepo struct {
 	Repo string
 	Rev  string
-	
+
 	SourceDir string
 
 	Output io.Writer
@@ -18,35 +19,27 @@ type bzrRepo struct {
 func newBzrRepo(repo, rev, workingDir string, output io.Writer) *bzrRepo {
 	r := new(bzrRepo)
 	r.Repo = repo
-	r.Rev = rev 
+	r.Rev = rev
 	r.SourceDir = filepath.Join(workingDir, "src")
 	r.Output = output
 	return r
 }
 
 func (r *bzrRepo) checkout() error {
-	args :=[]string{"branch", "-v"}
-	if r.Rev !=  "" {
+	args := []string{"branch", "-v"}
+	if r.Rev != "" {
 		args = append(args, "-r", r.Rev)
 	}
 	args = append(args, r.Repo)
 	args = append(args, r.SourceDir)
-	
+
 	cmd := exec.Command("bzr", args...)
 	cmd.Stdout = r.Output
 	cmd.Stderr = r.Output
-	
-	return cmd.Run()
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return os.RemoveAll(filepath.Join(r.SourceDir, ".bzr"))
 }
-
-		
-		
-	
-
-
-
-
-
-
-
-
